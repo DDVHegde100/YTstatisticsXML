428daf26ce1fd53fd2d77cb78bd8962b5c862533
DDVHegde100 <69629876+DDVHegde100@users.noreply.github.com>
2021-07-29 09:33:26 -0400

    Add files via upload

---
+++ main (5).cpp
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<PeopleOrganizationSystem>
+<person>
+    <name>Arun Maini</name>
+    <subscriberCount>
+     7.83 million   
+   </subscriberCount>
+    <genre>Technology</genre>
+</person>
+<person>
+    <name>JJ Olatunji</name>
+    <subscriberCount>
+    13.6 million
+    </subscriberCount>
+    <genre>Reddit?</genre>
+</person>
+<person>
+    <name>Marques Brownlee</name>
+    <subscriberCount>
+    14.4 million
+    </subscriberCount>
+    <genre>Technology</genre>
+</person>
+<person>
+    <name>Jonk</name>
+    <subscriberCount>
+    33.1 thousand
+    </subscriberCount>
+    <genre>Animation</genre>
+</person>
+</PeopleOrganizationSystem>
